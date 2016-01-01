93ef614fc1500942e9b1256da6f000e6eb60d2da
Nishi790 <rebelnishi@gmail.com>
2016-01-01 17:45:35 -0500

    created subclass of actions - spells made healing spell

---
+++ src/battleEngine/Spells.java
@@ -0,0 +1,42 @@
+package battleEngine;
+
+import characters.BattleObject;
+
+public class Spells extends Action {
+	private Spells(String name, int mp){
+		this.name=name;
+		magic=true;
+		cost=mp;
+	}
+	private void updateMP(){
+		int start=doer.getMP();
+		doer.setMP(start-cost);
+	}
+	class Heal extends Spells{
+		int health=0;
+		int lower;
+		int upper;
+		//lower and upper bounds of amount spell can heal
+		private Heal(String name, int mp, BattleObject caster, BattleObject target, int lower, int upper){
+			super(name, mp);
+			doer=caster;
+			this.target=target;
+			this.lower=lower;
+			this.upper=upper;
+		}
+		private void amountHealed(){
+			int amount=r.nextInt(upper-lower+2);
+			amount=amount+lower;
+			health=amount;
+		}
+		private void updateHP(){
+			int hp=target.getHP();
+			target.setHP(hp+health);
+		}
+		private void healingSpell(){
+			amountHealed();
+			updateHP();
+			updateMP();
+		}
+	}
+}
