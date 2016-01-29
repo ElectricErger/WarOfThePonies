e1340ac7a6a18c0a0f7c5233c70041f580bbc0af
Nishi790 <rebelnishi@gmail.com>
2016-01-29 14:34:25 -0500

    Methods to start/end battle, select next character whose turn it is, and implement AI if NPC Added inventory as parameter to BattleObject

--- src/battleEngine/Battle.java
+++ src/battleEngine/Battle.java
@@ -3,6 +3,7 @@ package battleEngine;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import mapEngine.Map;
 import characters.BattleObject;
@@ -10,14 +11,65 @@ import characters.BattleObject;
 public class Battle {
 	
 	private boolean inBattle;
-	
+	private BattleObject[] initiativeOrder;
+	private ArrayList<BattleObject> party;
+	private ArrayList<BattleObject> enemies;
 	
 	public void draw(Graphics g){
 		
 	}
 	
-	public void startBattle(ArrayList<BattleObject> party, Map location){}
-	
+	public void startBattle(ArrayList<BattleObject> party, ArrayList<BattleObject> enemies, Map location){
+		this.party=party;
+		this.enemies=enemies;
+		inBattle=true;
+		initiativeOrder=new BattleObject[enemies.size()+party.size()-2];
+		int i=0;
+		for(BattleObject b:party){
+			initiativeOrder[i]=b;
+			i++;
+		}
+		for(BattleObject b:enemies){
+			initiativeOrder[i]=b;
+			i++;
+		}
+		Arrays.sort(initiativeOrder);
+	}
+	public void endBattle(){
+		int battleExp=0;
+		for(BattleObject enemy: enemies){
+			battleExp=battleExp+enemy.getExp();
+		}
+		battleExp=battleExp/party.size();
+		for(BattleObject p:party){
+			p.setExp(p.getExp()+battleExp);
+			p.setExpToNextLevel(p.getExpToNextLevel()-battleExp);
+		}
+	}
+	public void selectAction(ArrayList<Action> menu, int i){
+		menu.get(i).execute();
+	}
+	public BattleObject nextAttacker(int a){
+		BattleObject next=initiativeOrder[a+1];
+		return next;
+	}
+	public void round(BattleObject current){
+		if(enemies.contains(current)){
+			AI enemy=new AI(current);
+			Action attack=enemy.choose(current.getAttacks(), party);
+			attack.execute();
+			int a=0;
+			for(BattleObject b: initiativeOrder){
+				if(b==initiativeOrder[a])break;
+				else a++;
+			}
+			round(nextAttacker(a));
+		}
+		else {
+			Menu menu=new Menu(current);
+			//Make a stupid menu to select from
+		}
+	}
 	public boolean inBattle(){ return inBattle; }
 	
 	public void keyDown(int key){
