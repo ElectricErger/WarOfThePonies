6e049757d9906dd2a94e2fd8af5fe81b16a59395
Max <electricerger@gmail.com>
2016-02-28 20:10:55 -0500

    Controls are wonky but it runs.

--- src/main/WoE.java
+++ src/main/WoE.java
@@ -1,40 +1,63 @@
 package main;
-import java.awt.Dimension;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
-
-import javax.swing.AbstractAction;
-import javax.swing.JComponent;
-import javax.swing.JFrame;
-import javax.swing.KeyStroke;
 
 import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
-public class WoE{
-	public static StateBasedGame game;
-	public static AppGameContainer woE;
-	public static final int FPS = 60;
+import state.StartScreen;
 
+public class WoE extends StateBasedGame{
+	private static StateBasedGame game;
+	private static AppGameContainer screen;
+	private static int width = 800, height = 600;
+	private static boolean fullScreen = false, vSynch = false;
+	private static final String gameName = "War for Equestria";
+	public static final int FPS = 60;
+	
+//Main method
 	public static void main(String[] args){
 		try{
-			game=new Panel();
-			woE=new AppGameContainer(game);
-			woE.setTitle("War for Equestria");
-			woE.setDisplayMode(800, 600, true);
-		//creates window and sets to fullscreen - put in the non-fullscreen basic dimensions you want
-		//to tell it how big the picture is supposed to be(e.g. how many tiles should fit)
-			woE.setTargetFrameRate(FPS);
-			//woE.setMinimumLogicUpdateInterval(10);
-			woE.start();
-		//starts game, calls init, render, then update, render, update, render etc
+			game=new WoE(); //The game
+			screen=new AppGameContainer(game); //The window
+			screen.setTitle(gameName);
+			screen.setDisplayMode(width, height, fullScreen); //Default
+			screen.setTargetFrameRate(FPS);
+			screen.start(); //starts game
+			//Init, render, update, render, update, etc
 		}
-		catch(SlickException e){
-			e.printStackTrace();
-		}
-		
+		catch(SlickException e){ e.printStackTrace(); }
+	}
+
+
+//Container object for game
+	private WoE(){
+		super (gameName);
+	}
+	public static AppGameContainer getScreen(){ return screen; }
+//Start thread
+	public void initStatesList(GameContainer container) throws SlickException {
+		addState(new StartScreen(this));
+		//MAIN GAME
+	}
+	
+	//Get and set screen aspects
+	public static int getWidth(){return width;}
+	public static int getHeight(){return height;}
+	public static boolean getFullScreen(){return fullScreen;}
+	public static boolean getVSync(){return vSynch;}
+	public static void setDimensions(int width, int height) throws SlickException{
+		WoE.width = width; WoE.height = height;
+		screen.setDisplayMode(width, height, fullScreen);
+	}
+	public static void setFullScreen(boolean fS) throws SlickException{
+		WoE.fullScreen = fS;
+		screen.setDisplayMode(width, height, fS);
+	}
+	public static void setVSync(boolean vS){
+		WoE.vSynch = vS;
+		screen.setVSync(vS);
 	}
 
 }
